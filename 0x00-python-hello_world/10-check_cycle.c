4e109304f4481b47dee86be367b7b7ff0147ae64
eyassine <leetyacine@gmail.com>
2024-03-12 02:39:51 +0000

    check for cycle linked list using tortoise and hare algorithm

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,10 +12,10 @@ int check_cycle(listint_t *list)
 {
 	listint_t *hare;
 
-	if (!list)
+	if (!list || !list->next)
 		return (0);
 	hare = list;
-	while (list && hare)
+	while (list && hare && hare->next)
 	{
 		list = list->next;
 		hare = hare->next->next;
