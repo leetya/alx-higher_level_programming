bad96966a644bdb60b4705d53c8d2df66396bc66
eyassine <leetyacine@gmail.com>
2024-03-13 15:45:11 +0000

    0-9

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+for x in range(0, 100):
+    if x < 10 :
+        print("0{0},".format(x), end=" ")
+    else :
+        if x == 99:
+            print("{0}".format(x))
+        else:
+            print("{0},".format(x), end=" ")
