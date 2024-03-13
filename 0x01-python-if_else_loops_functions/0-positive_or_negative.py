362715d0542fdcaae7a42dbed21fe72f7a8cf5c6
eyassine <leetyacine@gmail.com>
2024-03-13 14:44:34 +0000

    ternary op in python

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10, 10)
+number_status = ["is zero", "is negative", "is positive"]
+print(number_status[number if number == 0 else 2 if number > 0 else 1])
