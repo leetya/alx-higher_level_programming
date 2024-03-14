6235e9dc5b615e2fbb1abf1aa67fcc75e46b84c1
eyassine <leetyacine@gmail.com>
2024-03-14 14:21:35 +0000

    weird uppercase method

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if ord(c) > 96 and ord(c) < 123:
+        return (True)
+    return (False)
+
+def uppercase(str):
+    for x in str[:-1]:
+        print("{0}".format(x) if not islower(x)
+              else "{0}".format(chr(ord(x) - 32)), end='')
+    print("{0}".format(str[-1:]) if not islower(str[-1:])
+          else "{0}".format(chr(ord(str[-1:]) - 32))) 
+
