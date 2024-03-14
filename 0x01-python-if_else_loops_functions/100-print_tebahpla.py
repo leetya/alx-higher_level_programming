d26167e8d43659079f0c7be4d9b993ca52c5f526
eyassine <leetyacine@gmail.com>
2024-03-14 23:41:54 +0000

    14

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,7 +2,8 @@
 
 def convert_and_print():
     for x in range(90, 64, -1):
-        print("{0}".format(chr(x)) if x % 2 
-              else "{0}".format(chr(x + 32)) , end='')
+        print("{0}".format(chr(x)) if x % 2
+              else "{0}".format(chr(x + 32)), end='')
+
 
 convert_and_print()
